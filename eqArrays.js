09ac992e211e0532e3ebcca64b98c672c612d67c
mrjohnming <john.ngai@hotmail.com>
2022-01-12 20:46:01 +0000

    Refactored the eqArray function

--- eqArrays.js
+++ eqArrays.js
@@ -11,58 +11,38 @@ const assertEqual = (actual, expected) => {
 
 // Note: It's okay for eqArrays to not return true for nested arrays or arrays of objects that are identical. We will save this "deeper" problem for another day.
 
-/*
-Pseudocode/Steps:
-
-  1. Declare a function named eqArrays that accepts two parameters, array1 and array2.
-  
-    2. Declare a let variable named status with a boolean value of true.
-
-    3. If array1.length !== array2.length, then status = false,
-
-      4. Otherwise, create a for...in loop to iterate through the indexes (i) of array1.
-
-        5. If array1[i] !== array2[i], then status = false.
-
-    6. Return status;
-*/
-
 const eqArrays = (array1, array2) => {
-  let status = true;
-
   if (array1.length !== array2.length) {
-    status = false
+    return false;
   } else {
     for (const i in array1) {
       if (array1[i] !== array2[i]) {
-        status = false;
+        return false;
       }
     }
   }
-  return status;
+  return true;
 };
 
 // -----TEST CASES--START-----
+  // Expected Output: true
+  console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 
-// Expected Output: true
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+  // Expected Output: false
+  console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 
-// Expected Output: false
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+  // Expected Output: true
+  console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
 
-// Expected Output: true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+  // Expected Output: false
+  console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
 
-// Expected Output: false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+  // Expeceted Output: '...Assertion Passed...'
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
-// Expeceted Output: ...'Assertion Passed'...
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  // // Expeceted Output: '...Assertion Passed...'
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
 
-// // Expeceted Output: ...'Assertion Passed'...
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
-
-// // Expeceted Output: ...'Assertion Passed'...
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
-
+  // // Expeceted Output: '...Assertion Passed...'
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
 // -----TEST CASES--END-----
