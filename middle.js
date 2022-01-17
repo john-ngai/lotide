3ec4b3e9d7345dcb868bfdbf63dfdf811ed0d04e
mrjohnming <john.ngai@hotmail.com>
2022-01-17 19:08:35 +0000

    refactored and modularized the middle function

--- middle.js
+++ middle.js
@@ -1,24 +1,5 @@
-// Copied from eqArrays.js (12Jan2022 @ 3:47 PM)
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (const i in array1) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-// Copied from assertArraysEqual.js (12Jan2022 @ 3:48 PM)
-const assertArraysEqual = (array1, array2) => {
-  let status = eqArrays(array1, array2);
-  status ? 
-    console.log('Assertion Passed: array 1 === array 2') : 
-    console.log('Assertion Failed: array 1 !== array 2');
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 // CHALLENGE:
   // Implement middle which will take in an array and return the middle-most element(s) of the given array.
@@ -50,15 +31,4 @@ const middle = array => {
   }
 };
 
-// TEST CASES:
-  // An array that contains one element.
-  assertArraysEqual(middle([5]), []);
-
-  // An array that contains two elements.
-  assertArraysEqual(middle([5, 6]), []);
-
-  // An array with an even number of elements.
-  assertArraysEqual(middle([5, 6, 7, 8]), [6, 7]);
-
-  // An array with an odd number of elements.
-  assertArraysEqual(middle([5, 6, 7, 8, 9]), [7]);
+module.exports = middle;
