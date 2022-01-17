f021f466b04d0347d341d9c67bdb484db2245869
mrjohnming <john.ngai@hotmail.com>
2022-01-17 19:40:00 +0000

    refactored the middle function's test cases to use Mocha & Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,4 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
@@ -13,3 +14,25 @@ const middle = require('../middle');
 
   // // An array with an odd number of elements.
   assertArraysEqual(middle([5, 6, 7, 8, 9]), [7]);
+
+describe('#middle', () => {
+  it('returns an empty array when the input array contains one element', () => {
+    const arr = [5];
+    assert.deepEqual(middle(arr), []);
+  });
+
+  it('returns an empty array when the input array contains two element', () => {
+    const arr = [5, 6];
+    assert.deepEqual(middle(arr), []);
+  });
+
+  it('returns the two middle elements of an input array with an even number of elements', () => {
+    const arr = [5, 6, 7, 8];
+    assert.deepEqual(middle(arr), [6, 7]);
+  });
+
+  it('returns the single middle element of an input array with an odd number of elements', () => {
+    const arr = [5, 6, 7, 8, 9];
+    assert.deepEqual(middle(arr), [7]);
+  });
+});
