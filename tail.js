cf5387198c83f454c2f549bd5329ec4d1eb8ce07
mrjohnming <john.ngai@hotmail.com>
2022-01-17 18:46:51 +0000

    modularized the tail function and removed the test cases

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`❤️❤️❤️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👺🤮💩 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 // Create a function tail which returns the "tail" of an array:
 const tail = array => {
@@ -19,20 +13,4 @@ const tail = array => {
   return newArray;
 };
 
-// Test Case #1: Check to ensure that the original array has not be modified.
-const array1 = ['Light', 'House', 'Labs'];
-tail(array1);
-console.log(`Test Case #1:`);
-assertEqual(array1.length, 3); // Expected Output: 'Assertion Passed'
-console.log('\n');
-
-// Test Case #2: An array with only one element should yield an empty array for its tail.
-const array2 = ['Light'];
-console.log(`Test Case #2:`);
-console.log(tail(array2)); // Expected Output: []
-console.log('\n');
-
-// Test Case #3: An empty array should yield an empty array for its tail.
-const array3 = [];
-console.log(`Test Case #3:`); // Expected Output: []
-console.log(tail(array3));
+module.exports = tail;
